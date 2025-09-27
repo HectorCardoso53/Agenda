cad37f9945e28b7fd8a4033df310cc10576f85dd
Hector Araujo <hectorcardoso879@gmail.com>
2025-09-27 19:40:56 -0300

    Atualização docs.js e ajustes Firebase

--- js/script.js
+++ js/script.js
@@ -3,18 +3,8 @@
 // =============================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getFirestore, collection, doc, deleteDoc, updateDoc, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
-
+import { firebaseConfig } from '../config/firebaseConfig.js';
 // =============================
-// Configuração do Firebase
-// =============================
-const firebaseConfig = {
-  apiKey: "AIzaSyA7jUMn4ip8aDy3xxyJysq1wJRGiIrjZIQ",
-  authDomain: "agenda-78087.firebaseapp.com",
-  projectId: "agenda-78087",
-  storageBucket: "agenda-78087.firebasestorage.app",
-  messagingSenderId: "929555741792",
-  appId: "1:929555741792:web:66bd9aa8ac67cff6f29705"
-};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

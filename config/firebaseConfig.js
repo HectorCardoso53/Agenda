c69b1d8e335cee466e7de464af915b9eefd45bc2
Hector Araujo <hectorcardoso879@gmail.com>
2025-09-27 19:43:49 -0300

    Atualização completa do projeto para GitHub

---
+++ config/firebaseConfig.js
@@ -0,0 +1,8 @@
+  export const firebaseConfig = {
+          apiKey: "AIzaSyA7jUMn4ip8aDy3xxyJysq1wJRGiIrjZIQ",
+          authDomain: "agenda-78087.firebaseapp.com",
+          projectId: "agenda-78087",
+          storageBucket: "agenda-78087.firebasestorage.app",
+          messagingSenderId: "929555741792",
+          appId: "1:929555741792:web:66bd9aa8ac67cff6f29705"
+        };

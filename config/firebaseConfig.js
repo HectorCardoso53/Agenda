86cf7d1ac173998fc4e3b6cf89e8f77e9b33dc54
Hector Araujo <hectorcardoso879@gmail.com>
2025-10-01 17:42:52 -0300

    chore: remove firebaseConfig.js do versionamento

--- config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-  export const firebaseConfig = {
-          apiKey: "AIzaSyA7jUMn4ip8aDy3xxyJysq1wJRGiIrjZIQ",
-          authDomain: "agenda-78087.firebaseapp.com",
-          projectId: "agenda-78087",
-          storageBucket: "agenda-78087.firebasestorage.app",
-          messagingSenderId: "929555741792",
-          appId: "1:929555741792:web:66bd9aa8ac67cff6f29705"
-        };
